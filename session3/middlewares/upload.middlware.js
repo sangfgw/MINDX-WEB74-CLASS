d22a9e3be4345f3936c663c88024c97820443784
sangfgw <sang050901i@gmail.com>
2023-11-29 12:09:11 +0700

    Modify upload folder

--- session3/middlewares/upload.middlware.js
+++ session3/middlewares/upload.middlware.js
@@ -3,17 +3,19 @@ import multer from "multer";
 import e from "express";
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, path.resolve("upload/images"));
-    },
-    filename: function (req, file, cb) {
-        console.log(file);
-        const uniqueSuffix = Date.now();
-        cb(null, uniqueSuffix + "-" + file.originalname);
-    },
+  destination: function (req, file, cb) {
+    cb(null, path.resolve("upload/images"));
+  },
+  filename: function (req, file, cb) {
+    console.log(file);
+    const uniqueSuffix = Date.now();
+    cb(null, uniqueSuffix + "-" + file.originalname);
+  },
 });
-
 
 const MB_LIMIT = 1;
 const megabyteToByte = Math.pow(1024, 2) * MB_LIMIT;
-export const upload = multer({storage: storage, limits: {fileSize: megabyteToByte}});
+export const upload = multer({
+  storage: storage,
+  limits: { fileSize: megabyteToByte },
+});
